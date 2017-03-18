70c3512a3c47988bd9321e2a0ed87766416b4b6f
Octave Online <webmaster@octave-online.net>
2017-03-18 09:09:32 +0000

    Scripted user file commit

--- computeCost.m
+++ computeCost.m
@@ -13,8 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+h = theta' * X
 
-
+J = 1/(2*m) * (h - y) ^ 2
 
 
 % =========================================================================
