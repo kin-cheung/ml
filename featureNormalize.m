7275f792286c08b5c1cb3498bbf5ac3f3622e596
Octave Online <webmaster@octave-online.net>
2017-03-24 06:41:01 +0000

    Scripted auto-commit

--- featureNormalize.m
+++ featureNormalize.m
@@ -26,12 +26,13 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+mu = sum(X) ./ size(X, 1);
 
+m = size(X, 2);
+v = (sum(X - mu) .^ 2) ./ m
+sigma = v .^ (1/2);
 
-
-
-
-
+X_norm = (X - mu) / sigma;
 
 
 % ============================================================
