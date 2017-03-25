dad13f39f446d7653c32f1775d8fbacbf50b6036
Octave Online <webmaster@octave-online.net>
2017-03-25 09:20:50 +0000

    Scripted auto-commit

--- predict.m
+++ predict.m
@@ -15,11 +15,11 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+one = find(sigmoid(theta'*X) >= 0.5)
+zero = find(sigmoid(theta'*X) < 0.5)
 
-
-
-
-
+p(one) = 1
+p(zero) = 0
 
 % =========================================================================
 
