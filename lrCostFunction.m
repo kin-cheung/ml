9e3bbb594da420122a277454b87278575812512c
Octave Online <webmaster@octave-online.net>
2017-04-01 07:14:19 +0000

    Scripted auto-commit

--- lrCostFunction.m
+++ lrCostFunction.m
@@ -36,7 +36,9 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+J = (1/m) * sum((-y .* log(h)) - ((1-y) .* log(1-h)));
 
 
 
